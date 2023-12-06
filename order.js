faca3458fa6d53bb3698db8548a2bcc6f918050e
nham dinh <nham.nguyen@uptempo-global.com>
2023-12-06 17:06:46 +0700

    Initial commit

---
+++ order.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  `server order is running port 3000`;
+});
---
+++ payment.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.listen(3001, () => {
+  `server payment is running port 3001`;
+});
